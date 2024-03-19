e5d68423d0e9cbdc5e39899696d50e4d04d6e080
Jeffrey <1115378579@qq.com>
2024-03-19 17:21:02 +0800

    feat: apple游戏图片保存

--- src/get-img/index.js
+++ src/get-img/index.js
@@ -3,18 +3,43 @@ import { fileURLToPath } from 'node:url'
 import path, { dirname } from 'node:path'
 import XLSX from 'xlsx'
 import puppeteer from 'puppeteer'
+import axios from 'axios'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const browser = await puppeteer.launch({ 'headless': false, '--no-sandbox': true })
+const browser = await puppeteer.launch({ headless: false })
 const page = await browser.newPage()
-const xlsxData = readXLSX('../../files/3.7游戏站(1)(1).xlsx')
-for (const item of [xlsxData[0]]) {
-  await page.goto(item[' app store链接'])
-  const a = '1'
+const xlsxData = readXLSX('../../files/3.7游戏站(1).xlsx')
+for (const item of xlsxData) {
+  try {
+    await page.goto(item['app store链接'])
+    await page.waitForSelector('#ember3 source')
+
+    const srcset = await page.evaluate(() => {
+      // Replace 'selector' with the CSS selector of your element
+      const element = document.querySelector('#ember3 source')
+      if (element)
+        return element.srcset
+      else
+        return null
+    })
+    const imgPaht = srcset ? `${formatUrl(srcset)}/492x0w.webp` : ''
+    console.log(imgPaht)
+    saveUrlAsImage(path.join(__dirname, `../../files/apple/${item.__EMPTY}.png`), imgPaht)
+  }
+  catch (e) {
+    console.log(e)
+  }
 }
 
+function formatUrl(url) {
+  if (!url)
+    return ''
+  const urlArray = url.split('.webp')[0].split('/')
+  urlArray.pop()
+  return urlArray.join('/')
+}
 // // other actions...
 // await browser.close()
 function readXLSX(elxspath) {
@@ -23,5 +48,15 @@ function readXLSX(elxspath) {
     const workbook = XLSX.readFile(filePath)
     const sheet_name_list = workbook.SheetNames
     return XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list])
+  }
+}
+async function saveUrlAsImage(outputPath, url) {
+  try {
+    const data = await axios.get(url, { responseType: 'arraybuffer' })
+    fs.writeFileSync(outputPath, data.data)
+    console.log(`Image saved successfully at ${outputPath}`)
+  }
+  catch (error) {
+    console.error('Error saving image:', error)
   }
 }
